8a0dbf6830fbfcee7af4c99be01b04881fc3dc2b
Phùng Tiến Đạt <96523185+Puda14@users.noreply.github.com>
2022-11-16 08:35:53 +0700

    Create stackChar.c

---
+++ week4/stackChar.c
@@ -0,0 +1,24 @@
+/*Test kiểu stack bạn đã định nghĩa
+trong 1 chương trình mà nhận từ
+người sử dụng 1 xâu, và đảo ngược
+nó.*/
+#include <stdio.h>
+#include "stack.h"
+int main(){
+    int i = 0 , length = 0;
+    StackType s;
+    initialize(&s);
+    char c='\0';
+    while(c!='\n')
+    {
+        scanf("%c",&c);
+        push(c,&s);
+        length++;
+    }
+    while(!empty(s))
+    {
+        Eltype e = pop(&s);
+        printf("%c",e);
+    }
+    return 0;
+}
